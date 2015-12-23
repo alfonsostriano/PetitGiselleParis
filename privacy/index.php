ddada6e2b4dafa0cbf9833bbce2680a62577ba63
alfonsostriano <alfonso.striano@gmail.com>
2015-12-23 19:26:08 +0100

    Init PetitGiselleParis

---
+++ privacy/index.php
@@ -0,0 +1,162 @@
+<? require_once "../ccookie.php"; ?>
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en" >
+<!--<![endif]-->
+<head>
+<link rel="stylesheet" href="css/bootstrap.css" />
+<link rel="stylesheet" href="css/bootstrap-theme.css" />
+<link rel="stylesheet" href="css/style.css" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+<!-- Popup -->
+<script src="js/jquery-1.8.2.min.js"></script>
+<script src="js/jquery.popupoverlay.js"></script>
+<!--[if lt IE 9]>
+		<script src="js/html5shiv.js" type="text/javascript"></script>
+		<script src="js/respond.min.js" type="text/javascript"></script>
+	    <![endif]-->
+<!-- Popup -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Consales Arredamenti | Guida pratica all'acquisto del divano</title>
+ <!-- Favicon -->
+  <link rel="shortcut icon" href="../favicon.ico" />
+  <link rel="icon" href="../favicon.ico" />
+</head>
+<body>
+<div class="top_header">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1 class="colorwhite wgt600 text-center"> TESSUTO ANTIMACCHIA? Con questo risolvi, <span class="underline"> per sempre</span> , in modo facile e veloce, qualsiasi problema di macchia legato a olio, grasso, cocacola, caffè, inchiostro, vino, cioccolato, latte etc. etc.
+<br />
+        </h1>
+      </div>
+    </div>
+  </div>
+</div>
+<img src="images/shadow.png" class="img-responsive center-block" />
+<div class="container content_area">
+  <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+      <h2 class="wgt600 text-center">D' ora in poi  <span class="colorred wgt700">non avrai più bisogno </span> di coprire il tuo divano con foulard, copridivano, finalmente potrai goderti i tuoi momenti di relax in tutta tranquillità, anche se hai bambini e amici a casa.<br />
+      </h2>
+    </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12"></div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 margin_top2">
+    
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+            	<img src="images/icon1.png" class="img-responsive"/>
+            </div>
+            
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-9">
+            	<h5 class="margin_left">Con un semplice gesto, in 5 secondi elimini ogni tipo di macchia.</h5>
+            </div>
+      </div>
+      
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box ">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+            	<img src="images/icon2.png" class="img-responsive"/>
+            </div>
+            
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-9">
+            	<h5 class="margin_left">Basta un semplice panno imbevuto d'acqua per eliminare anche le macchie più difficili.</h5>
+            </div>
+      </div>
+      
+       <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box ">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+            	<img src="images/icon3.png" class="img-responsive"/>
+            </div>
+            
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-9">
+            	<h5 class="margin_left">Finalmente potrai tranquillamente invitare amici a casa e far giocare i tuoi bambini senza avere più l'assillo che si possa sporcare e rovinare il divano.</h5>
+            </div>
+      </div>
+      
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box ">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+            	<img src="images/icon4.png" class="img-responsive"/>
+            </div>
+            
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-9">
+            	<h5 class="margin_left">Non dovrai più sfoderare e rifoderare il divano, con la fatica che ne consegue. Fra l' altro, le cerniere si rompono anche spesso.</h5>
+            </div>
+      </div>
+      
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 box ">
+      		<div class="col-lg-2 col-md-2 col-sm-2 col-xs-3">
+            	<img src="images/icon5.png" class="img-responsive"/>
+            </div>
+            
+            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-9">
+            	<h5 class="margin_left">E' stato già testato da 165 persone, con risultati sorprendenti.</h5>
+            </div>
+      </div>
+      
+      
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 margin_top2"> <img src="images/report.png" class="center-block" /> </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12"> </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-2 clear margin_top6"> <img src="images/arrow_left.png" class="img-responsive pull-right" /> </div>
+    <a class="access basic_open" href="#basic">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-8 access_button margin_top4">
+      <h1 class="wgt700 text-center">Scarica la Guida, è GRATIS!</h1>
+    </div>
+    </a>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-2 margin_top6"> <img src="images/arrow_right.png" class="img-responsive" /> </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-2 clear"></div>
+    <div class="col-lg-11 col-md-11 col-sm-11 col-xs-10 margin_top4 margin_strip"> <br />
+      Ti auguro una splendida giornata!
+      <div class="handsean">Giulio Consales</div>
+      <br />
+    </div>
+  </div>
+</div>
+<div class="bottom_footer">
+  <div class="container">
+   
+  <h5 class="text-center">2015 © ConsalesArredamenti N. REA CE 232157 P.IVA 03287360618 | <a href="privacy.php" target="_blank">Privacy</a> | Via F.lli Bencivenga 35 Mondragone | tel 0823/978804</h5>
+      </div>
+      
+      <div class="col-lg-half col-md-half col-sm-half col-xs-12"></div>
+    </div>
+  </div>
+</div>
+</div>
+<div id="basic" class="well" style="max-width:44em;">
+  <div class="hs_button_wrapper">
+    <div class="hs_mid_container">
+      <div id="basic-modal-content"><span class="close_button"></span>
+        <h4 class="text-center">Scrivi il tuo Nome & tua Email</h4>
+        <div class="hs_form_div">
+          <div class="AW-Form-1123013257"></div>
+          <!-- SendinBlue Signup Form HTML Code -->
+          <iframe width="390" height="314" src="https://my.sendinblue.com/users/subscribe/js_id/262o2/id/10" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;"></iframe><p><center><img src="../images/lucchetto.png" alt="privacy" style="max-width: 16px; max-height: 16px;">  100% Sicuro. Odio lo Spam quanto te.</p></center>
+
+          <!-- End : SendinBlue Signup Form HTML Code -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+  <br />
+</div>
+<!-- Script -->
+<!--pou up -->
+<script>
+$(document).ready(function () {
+    $.fn.popup.defaults.pagecontainer = '.container'
+});
+</script> 
+<script>
+$(document).ready(function () {
+  $('#basic').popup();
+});
+</script>
+</body>
+</html>
+<? ccookie(); ?>
